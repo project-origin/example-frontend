e956b69e3f06ea9ed6a39338c5e9e75abbc20cd4
Martin Schmidt <martin@scoding.dk>
2020-04-26 10:05:19 +0000

    Updated to new uri

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class SettingsService {
 
-  apiBaseUrl = 'http://localhost:8081';
+  apiBaseUrl = 'http://api.app.eloprindelse.dk';
   authUrl = this.apiBaseUrl + '/auth/login';
 
   constructor() { }
